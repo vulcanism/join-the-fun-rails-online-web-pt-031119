e9d4788c58915044a53cf9645a793e19f4b72f2d
Savannah C <sgcorrell@gmail.com>
2019-07-31 19:12:00 +0000

    Automatically backed up by Learn

--- db/migrate/20190731191144_join_table_taxis_passengers.rb
+++ db/migrate/20190731191144_join_table_taxis_passengers.rb
@@ -1,4 +1,8 @@
 class JoinTableTaxisPassengers < ActiveRecord::Migration[5.0]
   def change
+    create_table :taxis_passengers do |t|
+      t.integer :taxi_id
+      t.integer :passenger_id
+    end
   end
 end
