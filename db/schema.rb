b07973349ae894f35dc102eb3e11c06cad13fd2e
Savannah C <sgcorrell@gmail.com>
2019-07-31 19:12:17 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20190731191144) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   create_table "taxis", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "taxis_passengers", force: :cascade do |t|
+    t.integer "taxi_id"
+    t.integer "passenger_id"
   end
 
 end
